598a951c2023f10aa3202df7280eabc2c45c7ee1
edoardob90 <edoardob90@gmail.com>
2023-03-24 16:14:08 +0100

    More cleanup and formatting

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,13 +1,18 @@
+# pylint: disable=missing-docstring, unused-argument, wrong-import-position, invalid-name
 from tests.testsuite import SubmissionTest
+
 
 class Exercise1Test(SubmissionTest):
     def setUp(self) -> None:
         super().setUp()
+        self.input = 0
+        self.output = 1
 
     def test_one(self):
-        self.assertEqual(self.fun(self.input), 1, msg="f(2) should return 1")
+        self.assertEqual(
+            self.fun(self.input), self.output, msg=f"Expected result is {self.output}"
+        )
 
     def test_two(self):
         with self.assertRaises(TypeError):
             self.fun()
-
--- tests/testsuite.py
+++ tests/testsuite.py
@@ -1,18 +1,22 @@
+# pylint: disable=missing-docstring, unused-argument, wrong-import-position, invalid-name
 import io
 import unittest
 from types import ModuleType
-from typing import Any, Callable, Dict, List, Type
+from typing import Callable, List, Type
 
-from IPython.core import guarded_eval
 from IPython.core.interactiveshell import InteractiveShell
-from IPython.core.magic import (Magics, cell_magic, line_cell_magic,
-                                line_magic, magics_class, needs_local_scope)
-from IPython.core.magic_arguments import (argument, magic_arguments,
-                                          parse_argstring)
+from IPython.core.magic import (
+    Magics,
+    cell_magic,
+    line_magic,
+    magics_class,
+    needs_local_scope,
+)
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
 from IPython.display import HTML, DisplayHandle, display
 
 
-def find_class(ns: ModuleType, name: str) -> Type:
+def find_class(namespace: ModuleType, name: str) -> Type:
     """
     Recursively find a class in a local namespace by name
     """
@@ -20,12 +24,9 @@ def find_class(ns: ModuleType, name: str) -> Type:
     def find_rec(current: dict, components: List[str]) -> Type:
         if len(components) == 1:
             return getattr(current, components[0])
-        elif len(components) > 1:
-            module = getattr(current, components[0])
-            return find_rec(module, components[1::])
+        return find_rec(getattr(current, components[0]), components[1::])
 
-    components = name.split(".")
-    return find_rec(ns, components)
+    return find_rec(namespace, name.split("."))
 
 
 class SubmissionTest(unittest.TestCase):
@@ -47,10 +48,11 @@ class SubmissionTest(unittest.TestCase):
 
 
 def format_failures(result: unittest.TestResult) -> DisplayHandle:
-    result_text = "\n".join([res for case, res in result.failures if res is not None])
+    """Format results upon failures"""
+    result_text = "\n".join([res for _, res in result.failures if res is not None])
     return display(
         HTML(
-            f"""<font color=red>The solution is not correct, following tests failed:<div style="white-space:pre">{result_text}</div></font>"""
+            f"""<font color=red>The solution is not correct, the following tests failed:<div style="white-space:pre">{result_text}</div></font>"""
         )
     )
 
@@ -81,15 +83,15 @@ class TestMagic(Magics):
         # Find the class in the current module
         test_class = find_class(self.shell.user_module, args.test)
         # Run cell
-        function_def = self.shell.ex(cell)
+        self.shell.ex(cell)
         # Extract the definition from the environment
-        fun = self.shell.user_ns[args.fun]
-        if fun is None:
-            raise ValueError(f"There is no function called {fun} in the scope")
-        # #Load the test suite
+        if (fun := self.shell.user_ns.get(args.fun)) is None:
+            raise ValueError(f"There is no function called '{args.fun}' in the scope")
+        # Load the test suite
         case_names = unittest.TestLoader().getTestCaseNames(test_class)
         cases = [test_class(fun, name) for name in case_names]
         suite = unittest.TestSuite(cases)
+        # Run the test suite and print results
         with io.StringIO() as stream:
             runner = unittest.TextTestRunner(stream=stream)
             result = runner.run(suite)
@@ -99,10 +101,7 @@ class TestMagic(Magics):
                     "<font color=green>Congratulations, your solution was correct</font>"
                 )
             )
-        else:
-            return format_failures(result)
-
-
+        return format_failures(result)
 
 
 def load_ipython_extension(ipython):
