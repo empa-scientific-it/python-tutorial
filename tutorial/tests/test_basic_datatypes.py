f3d6a34c9731a9942a4d92edb197bb5d0161a3fb
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2023-04-06 19:48:04 +0200

    Extend exercices for tests.

--- tutorial/tests/test_basic_datatypes.py
+++ tutorial/tests/test_basic_datatypes.py
@@ -1,11 +1,48 @@
 import pytest
 
-@pytest.mark.parametrize("set1, set2",
-    [
-        (1, 1)
-    ],
-)
-def test_sets_equal(set1, set2, function_to_test):
+SET1_2 = [
+    (set(), set()),
+    ({1, 2, 3}, {1, 2, 3}),
+    ({1, 2, 3}, {3, 2, 1}),
+    ({1, 2, 3}, {1, 2, 3, 4}),
+    ({1, 2, 3}, {1, 2}),
+    ({1, 2, 3}, {4, 5, 6}),
+    (set(), {1, 2, 3}),
+    ({1, 2, 3}, set()),
+]
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_union(set1, set2, function_to_test):
     """The test case(s)"""
-    assert (set1 == set2) == function_to_test(set1, set2)
-    
+    assert function_to_test(set1, set2) == set1.union(set2)
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_intersection(set1, set2, function_to_test):
+    """The test case(s)"""
+    assert function_to_test(set1, set2) == set1.intersection(set2)
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_difference(set1, set2, function_to_test):
+    """The test case(s)"""
+    assert function_to_test(set1, set2) == set1.difference(set2)
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_symmetric_difference(set1, set2, function_to_test):
+    """The test case(s)"""
+    assert function_to_test(set1, set2) == set1.symmetric_difference(set2)
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_subset(set1, set2, function_to_test):
+    """The test case(s)"""
+    assert function_to_test(set1, set2) == set1.issubset(set2)
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_superset(set1, set2, function_to_test):
+    """The test case(s)"""
+    assert function_to_test(set1, set2) == set1.issuperset(set2)
+
+@pytest.mark.parametrize("set1, set2", SET1_2)
+def test_sets_disjoint(set1, set2, function_to_test):
+    """The test case(s)"""
+    assert function_to_test(set1, set2) == set1.isdisjoint(set2)
+
