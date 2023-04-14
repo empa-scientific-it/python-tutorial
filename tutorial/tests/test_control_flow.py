c292fd86a8ab0e511f3f5148323ccdf8e6925746
edoardob90 <edoardob90@gmail.com>
2023-04-14 13:06:29 +0200

    Add two more exercises

--- tutorial/tests/test_control_flow.py
+++ tutorial/tests/test_control_flow.py
@@ -1,16 +1,63 @@
-from typing import List, Tuple
-import pytest
 import pathlib
+from collections import Counter
+from math import factorial
+from typing import List, Tuple
+
+import pytest
 
 
 def read_data(filename: str) -> str:
     """Read input data"""
-    # TODO: might need to fix the path
+    # NOTE: the path is relative to the IPython notebook's dir
     return pathlib.Path(f"tutorial/tests/data/{filename}").read_text()
 
 
 #
-# Exercise 1: Toboggan trajectory
+# Exercise 1: Find the factors
+#
+
+def is_prime(num: int) -> bool:
+    """One-liner (non-efficient) to check if a number is prime"""
+    return (factorial(num - 1) + 1) % num == 0
+
+
+def reference_solution_find_factors(num: int) -> List[int]:
+    """Dumb way to find the factors of an integer"""
+    if is_prime(num):
+        return [1, num]
+    return [m for m in range(1, num + 1) if num % m == 0]
+
+
+@pytest.mark.parametrize(
+    "num",
+    [350, 487, 965, 816, 598, 443, 13, 17, 211]
+)
+def test_find_factors(num: int, function_to_test) -> None:
+    assert function_to_test(num) == reference_solution_find_factors(num)
+
+
+#
+# Exercise 2: Cats with hats
+#
+
+def reference_solution_cats_with_hats() -> int:
+    """Solution with dictionaries"""
+    cats = {i: False for i in range(1, 101)}
+
+    for loop in range(1, 101):
+        for cat, has_hat in cats.items():
+            if cat % loop == 0:
+                cats[cat] = False if has_hat else True
+
+    return Counter(cats.values())[True]
+
+
+def test_cats_with_hats(function_to_test) -> None:
+    assert function_to_test() == reference_solution_cats_with_hats()
+
+
+#
+# Exercise 3: Toboggan trajectory
 #
 
 def parse_data(filename: str) -> List[List[int]]:
