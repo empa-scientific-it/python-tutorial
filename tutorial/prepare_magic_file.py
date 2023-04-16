ad1e11568afc5fe53b97f7ce65997b778d95c4a5
Simone Baffelli <simone.baffelli@gmail.com>
2023-04-16 13:06:41 +0200

    Input output (#37)
    * Added first version of functional programming
    
    * Added section on mapping and iteration.
    
    * Added more exercises
    
    * Removed functional programming from this branch
    
    * Added data and notebook for I/O tutorial.
    
    * Added new exercises
    
    * Added new exercises.
    
    * Added exercise on secret message.
    
    * Added section on paths.
    
    * Add FAQs
    
    * Update FAQ.md
    
    * Added testing tools.
    
    * Updated exercies
    
    * Added data.
    
    * Added pip requirements.
    
    * Removed wrong commit.
    
    * More robust lookup of test module file
    
    * Fixes VS Code behavior
    * Optionally specify the module name as the cell magic's argument
    
    * Added first slides.
    
    * Added testsuite import.
    
    * Added some helper files, finished tests.
    
    * Removed empty line from file.
    
    * Added information on how to run tests.
    
    * Added slides on file input.
    
    * Minor changes to FAQs
    
    * Moved data, changed hints in exercises.
    
    * Added unicode stuff.
    
    * Small fixes.
    
    * Fix bug of getting test module name from VS Code
    
    * Separated slides.
    
    * Fixed HTML.
    
    * Changed to we.
    
    * Changed capitalisation.
    
    * Fixed spelling.
    
    * Fixed spelling.
    
    * Fixed block on prompt.
    
    * Further fix to prompt block.
    
    * Fixed file IO spelling.
    
    * Fixed other typos in file IO
    
    * Corrected spelling in pathlib section.
    
    * Added example of path creation.
    
    * Changed text on glob.
    
    * Fixed list.
    
    * Update input_output.ipynb
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>
    
    * Update input_output.ipynb
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>
    
    * Update input_output.ipynb
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>
    
    * Update input_output.ipynb
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>
    
    * Update input_output.ipynb
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>
    
    * Changed cell type.
    
    * Fixed space.
    
    * Added TOC.
    
    * Moved slides.
    
    * Update index
    
    * Minor tweaks
    
    ---------
    
    Co-authored-by: Simone Baffelli <simone.baffelli@empa.ch>
    Co-authored-by: edoardob90 <edoardob90@gmail.com>
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>

---
+++ tutorial/prepare_magic_file.py
@@ -0,0 +1,53 @@
+import random
+
+def prepare_secret_file(file_path, secret_message, encoding='utf-8', random_data_length=10):
+    """
+    Prepare a binary file with a secret message encoded in it.
+    
+    Parameters:
+    file_path (str): The path to the binary file to create.
+    secret_message (str): The secret message to encode in the file.
+    encoding (str): The encoding to use for the secret message (default is 'utf-8').
+    random_data_length (int): The length of the random data to intersperse with the secret message (default is 10).
+    """
+    # Encode the secret message
+    encoded_message = secret_message.encode(encoding)
+    
+    # Generate some random data to intersperse with the secret message
+    random_data = bytes(random.sample(range(256), random_data_length))
+    
+    # Generate the binary data for the file
+    start_marker = b'\xff\xee\xdd\xcc\xbb\xaa'
+    end_marker = b'\xaa\xbb\xcc\xdd\xee\xff'
+    binary_data = random_data + start_marker + encoded_message + end_marker + random_data
+    
+    # Write the binary data to the file
+    with open(file_path, 'wb') as f:
+        f.write(binary_data)
+
+
+def decode_secret_message(file_path, encoding='utf-8', start_marker=b'\xff\xee\xdd\xcc\xbb\xaa', end_marker=b'\xaa\xbb\xcc\xdd\xee\xff'):
+    """
+    Decode the secret message from a binary file.
+    
+    Parameters:
+    file_path (str): The path to the binary file to read.
+    encoding (str): The encoding to use for the decoded message (default is 'utf-8').
+    start_marker (bytes): The byte sequence that marks the beginning of the secret message (default is b'\xff\xee\xdd\xcc\xbb\xaa').
+    end_marker (bytes): The byte sequence that marks the end of the secret message (default is b'\xaa\xbb\xcc\xdd\xee\xff').
+    
+    Returns:
+    str: The decoded secret message.
+    """
+    # Read the binary data from the file
+    with open(file_path, 'rb') as f:
+        binary_data = f.read()
+    
+    # Find the start and end positions of the secret message
+    start_pos = binary_data.find(start_marker) + len(start_marker)
+    end_pos = binary_data.find(end_marker, start_pos)
+    
+    # Decode the secret message
+    decoded_message = binary_data[start_pos:end_pos].decode(encoding)
+    
+    return decoded_message
