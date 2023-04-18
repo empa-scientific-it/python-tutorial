4ce21b5b9e1e9c3fcd92c100f4b8b39939367032
edoardob90 <edoardob90@gmail.com>
2023-04-18 17:14:10 +0200

    Add third exercise

--- tutorial/tests/test_object_oriented_programming.py
+++ tutorial/tests/test_object_oriented_programming.py
@@ -1,4 +1,9 @@
+import pathlib
+import sys
 from typing import List
+
+import pytest
+
 
 #
 # Exercise 1: Ice cream scoop
@@ -44,3 +49,48 @@ def test_ice_cream_bowl(function_to_test) -> None:
     scoops = [Scoop(flavor) for flavor in flavors]
     bowl.add_scoops(*scoops)
     assert function_to_test(flavors) == str(bowl)
+
+
+#
+# Exercise 3: Intcode computer
+#
+
+def read_data(name: str, data_dir: str = "data") -> pathlib.Path:
+    """Read input data"""
+    current_module = sys.modules[__name__]
+    return (pathlib.Path(current_module.__file__).parent / f"{data_dir}/{name}").resolve()
+
+
+class Computer:
+    """An Intcode computer class"""
+
+    def __init__(self, program: str):
+        self.program = [int(c.strip()) for c in program.split(',')]
+        self._backup = self.program[:]
+
+    def reset(self):
+        self.program = self._backup[:]
+
+    def run(self, pos=0):
+        while True:
+            if self.program[pos] == 99:
+                break
+            op1, op2 = self.program[self.program[pos + 1]], self.program[self.program[pos + 2]]
+            func = self.program[pos]
+            self.program[self.program[pos + 3]] = op1 + op2 if func == 1 else op1 * op2
+            pos += 4
+
+
+intcodes = [
+    "1,0,0,0,99",
+    "2,3,0,3,99",
+    "1,1,1,4,99,5,6,0,99"
+]
+intcodes += [read_data(f"intcode_{i}.txt").read_text() for i in (1, 2)]
+
+
+@pytest.mark.parametrize("intcode", intcodes)
+def test_intcode_computer(intcode: str, function_to_test) -> None:
+    computer = Computer(intcode)
+    computer.run()
+    assert function_to_test(intcode) == computer.program[0]
