4aab90f4df953d8b849f5f6d9733a8457a68c41e
Edoardo Baldi <edoardob90@gmail.com>
2025-05-08 16:28:38 +0200

    Add auto-import statement for IPython

--- docker/ipython_config.py
+++ docker/ipython_config.py
@@ -3,3 +3,8 @@ c = get_config()  # noqa # type: ignore
 
 # Automatically load testsuite extension
 c.InteractiveShellApp.extensions = ["tutorial.tests.testsuite"]
+
+# Automatically import some modules
+c.InteractiveShellApp.exec_lines = [
+    "from typing import Any, Callable, Dict, List, Optional, Tuple, Union"
+]
